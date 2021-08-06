a0f0891371f728c01449fd139bdce307b8c6023f
Ashley T Shumba <ashleytshumba@gmail.com>
2021-08-06 09:57:47 +0200

    remove unnecessary functions

--- lib/src/get_user_media_sample.dart
+++ lib/src/get_user_media_sample.dart
@@ -1,9 +1,6 @@
 import 'dart:core';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
-import 'package:path_provider/path_provider.dart';
 
 /*
  * getUserMedia sample
@@ -19,9 +16,6 @@ class _GetUserMediaSampleState extends State<GetUserMediaSample> {
   MediaStream? _localStream;
   final _localRenderer = RTCVideoRenderer();
   bool _inCalling = false;
-  bool _isTorchOn = false;
-  MediaRecorder? _mediaRecorder;
-  bool get _isRec => _mediaRecorder != null;
 
   List<MediaDeviceInfo>? _mediaDevicesList;
 
@@ -87,53 +81,6 @@ class _GetUserMediaSampleState extends State<GetUserMediaSample> {
     }
   }
 
-  void _startRecording() async {
-    if (_localStream == null) throw Exception('Stream is not initialized');
-    if (Platform.isIOS) {
-      print('Recording is not available on iOS');
-      return;
-    }
-    // TODO(rostopira): request write storage permission
-    final storagePath = await getExternalStorageDirectory();
-    if (storagePath == null) throw Exception('Can\'t find storagePath');
-
-    final filePath = storagePath.path + '/webrtc_sample/test.mp4';
-    _mediaRecorder = MediaRecorder();
-    setState(() {});
-
-    final videoTrack = _localStream!
-        .getVideoTracks()
-        .firstWhere((track) => track.kind == 'video');
-    await _mediaRecorder!.start(
-      filePath,
-      videoTrack: videoTrack,
-    );
-  }
-
-  void _stopRecording() async {
-    await _mediaRecorder?.stop();
-    setState(() {
-      _mediaRecorder = null;
-    });
-  }
-
-  void _toggleTorch() async {
-    if (_localStream == null) throw Exception('Stream is not initialized');
-
-    final videoTrack = _localStream!
-        .getVideoTracks()
-        .firstWhere((track) => track.kind == 'video');
-    final has = await videoTrack.hasTorch();
-    if (has) {
-      print('[TORCH] Current camera supports torch mode');
-      setState(() => _isTorchOn = !_isTorchOn);
-      await videoTrack.setTorch(_isTorchOn);
-      print('[TORCH] Torch state is now ${_isTorchOn ? 'on' : 'off'}');
-    } else {
-      print('[TORCH] Current camera does not support torch mode');
-    }
-  }
-
   void _toggleCamera() async {
     if (_localStream == null) throw Exception('Stream is not initialized');
 
@@ -141,27 +88,6 @@ class _GetUserMediaSampleState extends State<GetUserMediaSample> {
         .getVideoTracks()
         .firstWhere((track) => track.kind == 'video');
     await Helper.switchCamera(videoTrack);
-  }
-
-  void _captureFrame() async {
-    if (_localStream == null) throw Exception('Stream is not initialized');
-
-    final videoTrack = _localStream!
-        .getVideoTracks()
-        .firstWhere((track) => track.kind == 'video');
-    final frame = await videoTrack.captureFrame();
-    await showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-              content:
-                  Image.memory(frame.asUint8List(), height: 720, width: 1280),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: Navigator.of(context, rootNavigator: true).pop,
-                  child: Text('OK'),
-                )
-              ],
-            ));
   }
 
   @override
@@ -172,20 +98,8 @@ class _GetUserMediaSampleState extends State<GetUserMediaSample> {
         actions: _inCalling
             ? <Widget>[
                 IconButton(
-                  icon: Icon(_isTorchOn ? Icons.flash_off : Icons.flash_on),
-                  onPressed: _toggleTorch,
-                ),
-                IconButton(
                   icon: Icon(Icons.switch_video),
                   onPressed: _toggleCamera,
-                ),
-                IconButton(
-                  icon: Icon(Icons.camera),
-                  onPressed: _captureFrame,
-                ),
-                IconButton(
-                  icon: Icon(_isRec ? Icons.stop : Icons.fiber_manual_record),
-                  onPressed: _isRec ? _stopRecording : _startRecording,
                 ),
                 PopupMenuButton<String>(
                   onSelected: _selectAudioOutput,
